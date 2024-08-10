1fcb8f83a14e5b9f204cf4f322e6d1a4f70d6997
prakhar17072004 <pmankur2004@gmail.com>
2024-08-10 20:59:36 +0530

    operators

--- index.js
+++ index.js
@@ -10,6 +10,66 @@ const c=45;//single time decleration and single time intilizition and local scop
 //////////////////////////////////////////////////////////////
 //Identifier//
 // Identifier are the name of a class ,name of available ,name of a funtion , or name of any  other class member//
+////////////////////////////////////////////////////////
+
+//Operator //
+//operator are special symobols which is used to perform some specfic task//
+//Operends//
+//The value needed by an opertor is known as operends //
+
+//Types of operator//
+//Three types of operator are//
+//1.Unary operator//
+      //Two types//
+      //1.Logical not
+      //2.Increment/Decrement
+//2.Binary operator
+    //five operator
+    //1.Arithemetic operator
+    //2.Assigement operator
+    //3.Compound operator
+    //4.Relationl operator 
+    //5.Logical operator
+      // of two types
+      //1.Logical AND
+      //2.Logical OR/DOUBLE PIPELINE OPERATOR
+
+//3.Ternery operator
+    //1.CONDITIONAL OPERATOR
+
+////////////////////////////////////////////////////
+
+//Logical Not//
+
+    console.log(!false);
+    console.log(!true);
+    console.log(!0);
+    console.log(0);
+
+//////////////////////////////////////
+
+//Increment//
+  //preIncrement//
+let p=1;
+   ++p;
+console.log(p);
+console.log(++p);
+
+let k= 2;
+++k;
+++k;
+++k;
+++k;
+++k;
+++k;
+console.log(k);
+let g= ++k;
+++g;
+++g;
+++g;
+++g;
+console.log(g);
+console.log(a);
 
 
 

d63baa0debc7c6fef696759ed4a5bc2b7fadfec8
prakhar17072004 <pmankur2004@gmail.com>
2024-08-13 20:24:58 +0530

    added

---
+++ GCE/gce.js
@@ -0,0 +1,29 @@
+// console.log(a);
+// var a= 20;
+/////////////////////////////////////
+// console.log("start");
+let a =10;
+{
+    console.log(a)
+    let a=10;
+
+}
+console.log(a)
+console.log(b)
+console.log("end");
+
+/////////////////////////
+console.log("start");
+var b =20;
+const c= 30;
+{
+    
+    let a=100;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+
+}
+console.log(a)
+console.log(b)
+console.log("end");
